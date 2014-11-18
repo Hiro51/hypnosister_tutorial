d8912fc5c6464396ce3c47d5bda97e6f9226ac24
Hiro51 <h.tsujino0714@gmail.com>
2014-11-18 16:31:19 +0900

    Initial Commit

---
+++ Hypnosister_tutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/18/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister_tutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/18/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
